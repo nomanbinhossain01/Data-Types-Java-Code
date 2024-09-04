1b1bded44682ebac7f327e351c1599ca009bccc4
Noman Bin Hossain <nomanbinhossain01@gmail.com>
2024-09-05 01:02:33 +0600

    1st-commit two numbers and find their sum

---
+++ 10 Data Type Java/src/javaproblem1.java
@@ -0,0 +1,14 @@
+public class javaproblem1 {
+    public static void main(String[] args) {
+        Scanner sumInput=new Scanner(System.in);
+        System.out.println("Enter the valo off A: ");
+        int a=sumInput.nextInt();
+
+        System.out.println("enter the value of b :");
+        int b=sumInput.nextInt();
+        int sum=0;
+         sum=a+b;
+        System.out.println("sum of number is="+ sum);
+        
+    }
+}
