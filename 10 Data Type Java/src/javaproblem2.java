c8dbe32ee3359d86cf45ae201d467c7c674d3e83
Noman Bin Hossain <nomanbinhossain01@gmail.com>
2024-09-05 19:45:46 +0600

    2nd  commit arithmetic oparator

--- 10 Data Type Java/src/javaproblem2.java
+++ 10 Data Type Java/src/javaproblem2.java
@@ -1,3 +1,4 @@
+//Write a program to enter two numbers and perform all arithmetic operations.
 import java.util.Scanner;
 public class javaproblem2 {
     public static void main(String[] args) {
@@ -6,23 +7,20 @@ public class javaproblem2 {
         float a = input.nextInt();
         System.out.println("Enter B Number: ");
         float b = input.nextInt();
+        
         float sum=0,sub,div,multi,mod;
 
         sum = a + b;
-        System.out.println("Sum of a + b Number: " + sum);
-
         sub = a - b;
-        System.out.println("Sub of a - b Number: " + sub);
-
         multi = a * b ;
-        System.out.println("Multipication of a * b Number:" + multi);
-
         div = a / b ;
-        System.out.println("Division of a / b Number:" + div);
-
         mod = a % b ;
-        System.out.println("Mod of a % b Number:" + mod);
         
+        System.out.println("Sum of a + b Number: " + sum);
+        System.out.println("Sub of a - b Number: " + sub);
+        System.out.println("Multipication of a * b Number:" + multi);
+        System.out.println("Division of a / b Number:" + div);
+        System.out.println("Mod of a % b Number:" + mod);
 
     }
     
